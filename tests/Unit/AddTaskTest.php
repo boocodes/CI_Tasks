c59ffa3e0595ba7cb4d15d605cb7934b37b28f1a
Nick <117706748+boocodes@users.noreply.github.com>
2026-03-24 20:54:07 +0600

    Add files via upload

---
+++ tests/Unit/AddTaskTest.php
@@ -0,0 +1,28 @@
+<?php 
+
+use Task5\Application\Model\Task;
+use PHPUnit\Framework\TestCase;
+use Task5\Domain\Enums\TaskStatus;
+
+class AddTaskTest extends TestCase
+{
+    private Task $task;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->task = new Task();
+    }
+
+    public function mainTest(): void
+    {
+        $newTaskData = 
+        [
+            'title' => 'From test case!',
+            'description' => 'Description test case',
+            'status' => TaskStatus::NEW->value,
+        ];
+        $result = $this->task->add($newTaskData);
+        $this->assertTrue($result['id']);
+    }
+}
