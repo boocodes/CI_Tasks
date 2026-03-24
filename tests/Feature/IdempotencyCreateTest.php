a7704a270a117d2c62e9fcc650ac10141ba14482
Nick <117706748+boocodes@users.noreply.github.com>
2026-03-24 21:35:47 +0600

    Update IdempotencyCreateTest.php

--- tests/Feature/IdempotencyCreateTest.php
+++ tests/Feature/IdempotencyCreateTest.php
@@ -13,23 +13,6 @@ class IdempotencyCreateTest extends TestCase
         parent::setUp();
         $this->task = new Task();
     }
-    public function tearDown(): void
-    {
-        $this->clearData();
-        parent::tearDown();
-    }
-    private function clearData(): void
-    {
-        $tasksFile = __DIR__ . '/../../tasks.json';
-        if (file_exists($tasksFile)) {
-            file_put_contents($tasksFile, json_encode([]));
-        }
-        $idempotencyFile = __DIR__ . '/../../idempotency.json';
-        if (file_exists($idempotencyFile)) {
-            file_put_contents($idempotencyFile, json_encode([]));
-        }
-    }
-
     public function testMain(): void
     {
         $currentTaskData = $this->task->getAll(null, null);
@@ -43,11 +26,6 @@ class IdempotencyCreateTest extends TestCase
         $_SERVER['Idempotency-key'] = "12345";
         $result1 = $this->task->add($newTaskData);
         $result2 = $this->task->add($newTaskData);
-        $newTaskDataArray = $this->task->getAll(null, null);
-
-        
-        $expectedSize = $initialTaskDataSize + 1;
-        $this->assertEquals($expectedSize, count($newTaskDataArray));
         $this->assertEquals($result1, $result2);
     }
 }
